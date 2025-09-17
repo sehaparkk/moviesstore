92ab0d0601d2cb7a930005fc1834be27a98d84c9
Seha <spark993@gatech.edu>
2025-09-17 18:22:06 -0400

    Revert "watchlist feature"
    This reverts commit b64eedec9bac1ab28495fcd0e4174641d1421a55.

--- moviesstore/settings.py
+++ moviesstore/settings.py
@@ -41,7 +41,6 @@ INSTALLED_APPS = [
     'movies',
     'accounts',
     'cart',
-    'watchlist',
 ]
 
 MIDDLEWARE = [
--- watchlist/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('', views.index, name='watchlist.index'),
-    path('add/<int:movie_id>/', views.add_to_watchlist, name='watchlist.add'),
-    path('update_rating/<int:item_id>/', views.update_rating, name='watchlist.update_rating'),
-]
